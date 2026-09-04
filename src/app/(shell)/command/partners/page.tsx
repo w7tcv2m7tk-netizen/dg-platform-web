e83eb5fe5365aa05992e37ab513f4596d1217d9d
Ben Roe <311585786+w7tcv2m7tk-netizen@users.noreply.github.com>
2026-09-04 18:24:40 +1000

    Security: gate partner dashboard page

--- src/app/(shell)/command/partners/page.tsx
+++ src/app/(shell)/command/partners/page.tsx
@@ -1,10 +1,12 @@
-import { buildPartnerDashboardWorkspace } from "@dg/platform-core";
+import { getOperatorPartnerDashboardWorkspace } from "@dg/platform-core";
 
 import { PartnerProgrammeDashboard } from "@/components/partners/PartnerProgrammeDashboard";
+import { requirePlatformOperatorContext } from "@/lib/platform-operator";
 
 export const dynamic = "force-dynamic";
 
 export default async function PartnerProgrammeDashboardPage() {
-  const data = await buildPartnerDashboardWorkspace();
+  const operator = await requirePlatformOperatorContext();
+  const data = await getOperatorPartnerDashboardWorkspace(operator);
   return <PartnerProgrammeDashboard data={data} />;
 }
