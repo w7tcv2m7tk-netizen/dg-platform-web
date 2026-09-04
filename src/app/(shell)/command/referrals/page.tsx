60b24b0b80bb54e93b60208a5842d2ff1dd1b1de
Ben Roe <311585786+w7tcv2m7tk-netizen@users.noreply.github.com>
2026-09-04 18:31:30 +1000

    Security: gate partner referrals page

--- src/app/(shell)/command/referrals/page.tsx
+++ src/app/(shell)/command/referrals/page.tsx
@@ -1,10 +1,12 @@
-import { buildReferralsWorkspace } from "@dg/platform-core";
+import { getOperatorPartnerReferralsWorkspace } from "@dg/platform-core";
 
 import { PartnerReferralsDashboard } from "@/components/partners/PartnerReferralsDashboard";
+import { requirePlatformOperatorContext } from "@/lib/platform-operator";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminReferralsPage() {
-  const data = await buildReferralsWorkspace();
+  const operator = await requirePlatformOperatorContext();
+  const data = await getOperatorPartnerReferralsWorkspace(operator);
   return <PartnerReferralsDashboard data={data} />;
 }
