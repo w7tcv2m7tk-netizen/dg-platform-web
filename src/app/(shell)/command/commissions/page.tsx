cf7f3699aa6381f59c9a7ba364547d6408758d59
Ben Roe <311585786+w7tcv2m7tk-netizen@users.noreply.github.com>
2026-09-04 17:35:34 +1000

    Security: capability-gate commissions workspace

--- src/app/(shell)/command/commissions/page.tsx
+++ src/app/(shell)/command/commissions/page.tsx
@@ -1,10 +1,12 @@
-import { buildCommissionsWorkspace } from "@dg/platform-core";
+import { getOperatorCommissionsWorkspace } from "@dg/platform-core";
 
 import { PartnerCommissionsDashboard } from "@/components/partners/PartnerCommissionsDashboard";
+import { requirePlatformOperatorContext } from "@/lib/platform-operator";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminCommissionsPage() {
-  const data = await buildCommissionsWorkspace();
+  const operator = await requirePlatformOperatorContext();
+  const data = await getOperatorCommissionsWorkspace(operator);
   return <PartnerCommissionsDashboard data={data} />;
 }
